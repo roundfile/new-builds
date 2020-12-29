d6ffbdb27e970b06d8222563212abafa7bbce497
Dave <1648748+roundfile@users.noreply.github.com>
2020-12-29 11:14:33 -0800

    debug setup-mac3

--- src/build-mac3.sh
+++ src/build-mac3.sh
@@ -59,7 +59,4 @@ fi
 # distribution
 rm -rf build dist
 sleep .3 # sometimes it takes a little for dist to get really empty
-
-ls -l $PYTHON/bin/p*
-
 $PYTHON/bin/python$PYTHON_V setup-mac3.py py2app | egrep -v '^(creating|copying file|byte-compiling|locate)'
