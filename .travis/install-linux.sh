2639852ec94502d3394897b7b21e84f5f5665b06
Dave <1648748+roundfile@users.noreply.github.com>
2020-12-30 07:47:03 -0800

    debug install-linux.sh restore -y comment fakeroot

--- .travis/install-linux.sh
+++ .travis/install-linux.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 set -ex
-sudo apt-get update
-sudo apt-get install ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5-default
+sudo apt-get update -y -q
+sudo apt-get install -y -q ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5-default
+#sudo apt-get install -y -q fakeroot
 
 # add libs not installed by default on Qt5.15 any longer
-sudo apt-get install -y libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
+sudo apt-get install -y -q libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
 
 gem install fpm
-pip3 install --upgrade pip
-pip3 install -r src/requirements.txt
-pip3 install -r src/requirements-${ARTISAN_OS}.txt
+pip install --upgrade pip
+pip install -r src/requirements.txt
+pip install -r src/requirements-${ARTISAN_OS}.txt
 
 .travis/install-libusb.sh
 .travis/install-phidgets.sh
