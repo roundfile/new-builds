770feac7a77502a789e2c10220294d52a7fe82ae
Dave <1648748+roundfile@users.noreply.github.com>
2021-01-01 17:47:32 -0800

    debug

--- .ci/install-rpi.sh
+++ .ci/install-rpi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 set -ex
-sudo apt-get update
-sudo apt-get install p7zip-full libglib2.0-dev zlib1g-dev qemu
+sudo apt-get update -y -q
+sudo apt-get install -y -q p7zip-full libglib2.0-dev zlib1g-dev qemu
