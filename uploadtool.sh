248f2b38b7662d102d5710436fd5c1ab6f29825c
Dave <1648748+roundfile@users.noreply.github.com>
2021-01-12 10:42:05 -0800

    no artifacts

--- uploadtool.sh
+++ uploadtool.sh
@@ -34,8 +34,8 @@ if [ ! -z "$APPVEYOR" ]; then
     TRAVIS_COMMIT="$APPVEYOR_REPO_COMMIT"
     TRAVIS_JOB_ID="$APPVEYOR_JOB_ID"
     TRAVIS_BUILD_WEB_URL="${APPVEYOR_URL}/project/${APPVEYOR_ACCOUNT_NAME}/${APPVEYOR_PROJECT_SLUG}/build/job/${APPVEYOR_JOB_ID}"
-    if [ $inhibit_deploy == 'true' ] ; then
-      echo "Release uploading disabled"
+    if [[ $APPVEYOR_REPO_COMMIT_MESSAGE =~ nodeploy ]] || [[ $APPVEYOR_REPO_COMMIT_MESSAGE_EXTENDED =~ nodeploy ]] ; then
+      echo "Release uploading disabled, commit message contains 'nodeploy'"
       exit 0
     fi
     if [ ! -z "$APPVEYOR_PULL_REQUEST_NUMBER" ] ; then
