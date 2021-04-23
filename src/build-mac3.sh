a66781fb9db8ad0b6c78723160026832a0a44264
Dave <1648748+roundfile@users.noreply.github.com>
2021-04-23 12:10:12 -0700

    again

--- src/build-mac3.sh
+++ src/build-mac3.sh
@@ -8,7 +8,7 @@ set -e  # reduced logging
 if [ ! -z $APPVEYOR ]; then
     # Appveyor CI builds
     echo "NOTICE: Appveyor build"
-    export PYTHON=/Users/appveyor/venv3.9.1
+    export PYTHON=/Users/appveyor/venv3.9
     export PYTHONBIN=$PYTHON/bin
     export PYTHONPATH=$PYTHON/lib/python3.9
     export PYTHON_V=3.9
