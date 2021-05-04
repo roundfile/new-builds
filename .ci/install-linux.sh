bfbbcab0d24f0479175243bb1c8e2ad0217df451
Dave <1648748+roundfile@users.noreply.github.com>
2021-05-04 12:46:18 -0700

    move apt-get update nodeploy

--- .ci/install-linux.sh
+++ .ci/install-linux.sh
@@ -2,11 +2,11 @@
 
 set -ex
 #sudo apt-get update -y -q
-sudo apt-get install --fix-missing -y -q ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5-default
-sudo apt-get install --fix-missing -y -q fakeroot
+sudo apt-get install -y -q ruby-dev build-essential p7zip-full rpm gdb libudev-dev qt5-default
+sudo apt-get install -y -q fakeroot
 
 # add libs not installed by default on Qt5.15 any longer
-sudo apt-get install --fix-missing -y -q libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
+sudo apt-get install -y -q libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
 
 gem install fpm
 pip install --upgrade pip
